954de63cb1c7903205e3f53b5dca37770ce169e2
raha-jm <raha62092@gmail.com>
2025-11-02 11:35:25 +0330

    Add functionality to append new tasks to the list

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log('Todo Manager شروع شد')
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+const message = document.getElementById('message')
+addTaskBtn.addEventListener('click',function(){
+    const taskText = taskInput.nodeValue.trim();
+
+    if (taskText === "") {
+        message.innerHTML = "لطفا یک کار وارد کنید";
+        return;
+     }
+    const li = document.createElement("li");
+    li.textContent = taskText;
+    taskList.appendChild(li);
+
+    taskInput.nodeValue = "";
+})
